3d5c357a9e70ef0a10e08bf5d793e3000659d170
hsgamer <huynhqtienvtag@gmail.com>
2024-11-20 02:20:33 +0700

    improve getAll methods

--- src/main/java/me/hsgamer/demodatarest/controller/CrudController.java
+++ src/main/java/me/hsgamer/demodatarest/controller/CrudController.java
@@ -14,13 +14,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 public abstract class CrudController<T, ID, RP extends CrudRepository<T, ID>, REQ, RES> {
     protected final RP repository;
 
     protected CrudController(RP repository) {
         this.repository = repository;
+    }
+
+    protected static Sort getSort(List<String> sort) {
+        List<Sort.Order> orders = new ArrayList<>();
+        for (String s : sort) {
+            String[] split = s.split(",");
+            Sort.Direction direction = Sort.Direction.ASC;
+            if (split.length > 1) {
+                direction = Sort.Direction.fromString(split[1]);
+            }
+            Sort.Order order = new Sort.Order(direction, split[0]);
+            if (split.length > 2 && "ignoreCase".equalsIgnoreCase(split[2])) {
+                order = order.ignoreCase();
+            }
+            orders.add(order);
+        }
+        return Sort.by(orders);
+    }
+
+    protected static Pageable getPageable(int page, Integer size, Sort sort) {
+        return PageRequest.of(page, size == null ? 10 : size, sort == null ? Sort.unsorted() : sort);
     }
 
     protected abstract RES toResponse(T t);
@@ -30,52 +50,37 @@ public abstract class CrudController<T, ID, RP extends CrudRepository<T, ID>, RE
     protected abstract void updateEntity(T t, REQ req);
 
     @GetMapping
-    public ResponseEntity<ResponseDTO<List<RES>>> getAll(@RequestParam("page") @Nullable Integer page, @RequestParam("size") @Nullable Integer size, @RequestParam("sort") @Nullable List<String> sort) {
+    public ResponseEntity<ResponseDTO<List<RES>>> getAll(@RequestParam("page") @Nullable Integer page, @RequestParam("size") @Nullable Integer size, @RequestParam("sort") @Nullable List<String> sorts) {
         Map<String, Object> extra = new HashMap<>();
         List<RES> list = new ArrayList<>();
-        Consumer<T> addToList = t -> list.add(toResponse(t));
 
-        if (page != null && repository instanceof PagingAndSortingRepository) {
+        Sort sort = sorts == null ? null : getSort(sorts);
+        Pageable pageable = page == null ? null : getPageable(page, size, sort);
+
+        Iterable<T> iterable;
+        if ((pageable == null && sort == null) || !(repository instanceof PagingAndSortingRepository)) {
+            iterable = repository.findAll();
+        } else {
             //noinspection unchecked
             PagingAndSortingRepository<T, ID> pagingAndSortingRepository = (PagingAndSortingRepository<T, ID>) repository;
 
-            int finalPage = page;
-            int finalSize = size == null ? 10 : size;
-            Sort finalSort;
-            if (sort == null) {
-                finalSort = Sort.unsorted();
+            if (pageable != null) {
+                Page<T> pageResult = pagingAndSortingRepository.findAll(pageable);
+                iterable = pageResult;
+                extra.put("totalPages", pageResult.getTotalPages());
+                extra.put("totalElements", pageResult.getTotalElements());
+                extra.put("currentPage", pageResult.getNumber());
+                extra.put("currentElements", pageResult.getNumberOfElements());
+                extra.put("hasNext", pageResult.hasNext());
+                extra.put("hasPrevious", pageResult.hasPrevious());
             } else {
-                List<Sort.Order> orders = new ArrayList<>();
-                for (String s : sort) {
-                    String[] split = s.split(",");
-                    Sort.Direction direction = Sort.Direction.ASC;
-                    if (split.length > 1) {
-                        direction = Sort.Direction.fromString(split[1]);
-                    }
-                    Sort.Order order = new Sort.Order(direction, split[0]);
-                    if (split.length > 2 && "ignoreCase".equalsIgnoreCase(split[2])) {
-                        order = order.ignoreCase();
-                    }
-                    orders.add(order);
-                }
-                finalSort = Sort.by(orders);
+                iterable = pagingAndSortingRepository.findAll(sort);
             }
+        }
 
-            Pageable pageable = PageRequest.of(finalPage, finalSize, finalSort);
-
-            Page<T> pageResult = pagingAndSortingRepository.findAll(pageable);
-
-            pageResult.forEach(addToList);
-
-            extra.put("totalPages", pageResult.getTotalPages());
-            extra.put("totalElements", pageResult.getTotalElements());
-            extra.put("currentPage", pageResult.getNumber());
-            extra.put("currentElements", pageResult.getNumberOfElements());
-            extra.put("hasNext", pageResult.hasNext());
-            extra.put("hasPrevious", pageResult.hasPrevious());
-        } else {
-            Iterable<T> iterable = repository.findAll();
-            iterable.forEach(addToList);
+        for (T t : iterable) {
+            RES res = toResponse(t);
+            list.add(res);
         }
 
         return ResponseEntity.ok(new ResponseDTO<>(true, ResponseCode.SUCCESS, "OK", list, extra));
